ec5bc1e8ca36e3ec2dca7207e7be6645414e573c
Chung-Ying <ag.cyho@gmail.com>
2024-06-07 00:25:00 +0800

    feat: modify navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -12,17 +12,17 @@ const NavBar = () => {
   }
 
   return (
-    <nav className="relative w-screen border border-slate-800 bg-white lg:h-screen lg:w-1/4">
-      <div
-        className="flex justify-around border border-orange-300 py-3"
-        onClick={handleHamburger}
-      >
-        <div className="hamburger flex h-[1.5rem] w-[1.5rem] flex-col justify-between">
+    <nav className="relative w-screen bg-white lg:h-screen lg:w-1/4">
+      <div className="flex items-center justify-around py-5">
+        <div
+          className="hamburger flex h-[1.5rem] w-[1.5rem] flex-col justify-between lg:hidden"
+          onClick={handleHamburger}
+        >
           <div
             className={clsx(
               "h-1/6 w-full rounded-sm bg-black transition-transform",
               {
-                "translate-y-[0.5rem] rotate-45 transform": isHamburgerOpen
+                "translate-y-[0.625rem] rotate-45 transform": isHamburgerOpen
               }
             )}
           ></div>
@@ -38,24 +38,26 @@ const NavBar = () => {
             className={clsx(
               "h-1/6 w-full rounded-sm bg-black transition-transform",
               {
-                "-translate-y-[0.5rem] -rotate-45 transform": isHamburgerOpen
+                "-translate-y-[0.625rem] -rotate-45 transform": isHamburgerOpen
               }
             )}
           ></div>
         </div>
-        <h3>白頭翁不吃小米</h3>
+        <h3 className="text-xl font-bold lg:text-3xl">白頭翁不吃小米</h3>
         {/* logo here */}
         <Logo />
       </div>
       <ul
         className={clsx(
-          "absolute left-0 top-full flex w-full flex-col items-center gap-3 border border-lime-700 bg-white transition-all duration-300",
+          "flex w-full flex-col items-center gap-3 bg-white transition-all duration-300",
           {
             "max-h-0 opacity-0": !isHamburgerOpen,
             "max-h-[200px] opacity-100": isHamburgerOpen
           }
         )}
-        style={{ overflow: "hidden" }}
+        style={{
+          overflow: "hidden"
+        }}
       >
         <li>
           <a href="/">Home</a>
